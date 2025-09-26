fb75070beb1bb2c03a7fdb51049c20ff1329ff22
MishkatMukit <mahabubmishkat8@gmail.com>
2025-09-27 05:13:22 +0600

    task management javascript added for interactivity

--- script/task.js
+++ script/task.js
@@ -1,11 +1,33 @@
+const activityContainer  = document.getElementById('activity-container')
+const allBtn = document.getElementsByClassName('btn-task');
+let taskRemaining = parseInt(getInTextByID('task-remaining'));
+let taskCompleted = parseInt(getInTextByID('task-completed'));
 
 
-const allBtn=document.getElementsByClassName('btn-task');
-console.log(allBtn)
 for(const btn of allBtn){
     btn.addEventListener('click',
         function(event){
             const taskName = event.target.parentNode.parentNode.childNodes[3].innerText;
-            console.log(taskName);
+            event.target.disabled=true;
+            event.target.style.opacity=0.5;
+            const cTime= showTime();
+            const div = document.createElement('div');
+            div.innerHTML=`<div class="bg-[#F4F7FF] p-2 my-2 rounded-lg"><p>You have Complete The Task <b>${taskName}</b> at <span class="text-blue-700">${cTime}</span></div>`
+            
+            activityContainer.appendChild(div);
+            taskRemaining--;
+            taskCompleted++;
+            setInnerTextByIDandValue('task-remaining',taskRemaining)
+            setInnerTextByIDandValue('task-completed',taskCompleted)
+            alert(`Congratulations!
+You have completed the task : ${taskName}`)
+            if(taskRemaining===0){
+                alert("Congratulations! You have completed all task.")
+            }
+            
     })
 }
+document.getElementById('btn-clear').addEventListener('click', 
+    function(){
+        document.getElementById('activity-container').innerText='';
+})
--- script/utilities.js
+++ script/utilities.js
@@ -11,3 +11,31 @@ function setInnerTextByIDandValue(id, value){
 function handleToggle(id, status){
     document.getElementById(id).style.display = status;
 }
+function showTime() {
+  // Get the current date and time
+  let time = new Date();
+  let hour = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  let am_pm = "AM";
+
+  // Set AM/PM format
+  if (hour >= 12) {
+    if (hour > 12) {
+      hour -= 12;
+    }
+    am_pm = "PM";
+  } else if (hour == 0) {
+    hour = 12;
+    am_pm = "AM";
+  }
+
+  // Add a leading zero to single-digit numbers
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+
+  // Combine the time components into a string
+  let currentTime = hour + ":" + min + ":" + sec + " " + am_pm;
+  return currentTime;
+}
